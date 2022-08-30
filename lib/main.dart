d7460ad5558e2915b35d5dc0409b5670f3aebb08
Budi Prasetyo <b.prasetyo@eksad.com>
2022-08-30 10:50:08 +0700

    --

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:multi_cloudv3/dashboard/dashboard.dart';
 import 'package:multi_cloudv3/login.dart';
 import 'package:multi_cloudv3/screen/home.dart';
-// import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // configureApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  configureApp();
   runApp(const MyApp());
 }
 
-// void configureApp() {
-//   setUrlStrategy(PathUrlStrategy());
-// }
+void configureApp() {
+  setUrlStrategy(PathUrlStrategy());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
